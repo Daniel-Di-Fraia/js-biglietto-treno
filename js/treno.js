109067a70da8f8ce64974656643846849204ee85
Daniel-Di-Fraia <daniel.difraia2600@gmail.com>
2025-08-27 14:55:20 +0200

    variabili necessarie

--- js/treno.js
+++ js/treno.js
@@ -6,3 +6,21 @@ va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 Questo richiederà un minimo di ricerca. */
 
+
+// INPUT
+// chiedo all user di inserire la sua età e quanti KM deve fare
+const eta = parseInt(prompt("inserisci la tua età"));
+const km = parseInt(prompt("inserisci quanti Km devi fare"))
+console.log("età utente:", eta);
+console.log("Km da percorrere:", km);
+
+//condizioni per stabilire prezzo biglietto
+const priceKm = 0.21;
+
+
+
+
+
+
+
+
